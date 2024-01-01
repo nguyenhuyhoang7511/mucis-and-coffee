5062b912b60c0d4cb842920a3a3f7ed236a60efd
Nguyen Huy Hoang <hoangnh@Hoangs-MacBook-Pro.local>
2024-01-02 00:23:33 +0700

    Upload code f1

---
+++ src/@core/components/snackbar/SnackbarCustom.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+interface Props {
+    isShow: boolean;
+    message: string;
+    time?: string;
+    color?: string;
+    onClose: () => void;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+    <div class="text-start ma-2">
+        <v-snackbar :timeout="2000" v-model="props.isShow">
+            {{ props.message }}
+            <template #actions>
+                <v-btn color="pink" variant="text" @click="props.onClose">
+                    Close
+                </v-btn>
+            </template>
+        </v-snackbar>
+    </div>
+</template>
--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,57 +1,71 @@
 <script setup lang="ts">
-import { ErrorMessage, Field, Form } from 'vee-validate';
+import SnackbarCustom from "@/@core/components/snackbar/SnackbarCustom.vue";
+import { ErrorMessage, Field, Form } from "vee-validate";
 
-import * as yup from 'yup';
+import * as yup from "yup";
 const form = ref({
-  email: '',
-  userName: '',
-  password: '',
-  confirm: '',
+  email: "",
+  userName: "",
+  password: "",
+  confirm: "",
   agree: false,
-})
-
-const isPasswordVisible = ref(false)
-const isConfirmPasswordVisible = ref(false)
-
-const schema = yup.object({
-  email: yup.string().required("Email không được để trống").email("Email không đúng định dạng").matches(/@gmail\.com$/, "Email phải có định dạng là @gmail.com"),
-  userName: yup.string().required("Tên người dùng không được để trống")
-  ,
-  password: yup
-    .string()
-    .required('Mật khẩu không được để trống')
-    .min(8, 'Mật khẩu cần có tối thiểu 8 kí tự')
-    .matches(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/, 'Mật khẩu yêu cầu chứa cả chữ và số'),
-  confirmPassword: yup
-    .string()
-    .required('Xác nhận mật khẩu là bắt buộc')
-    .oneOf([yup.ref('password'), ''], 'Xác nhận mật khẩu không chính xác'),
-  agree: yup.boolean().oneOf([true], 'Bạn cần phải đồng ý với các điều kiện và điều khoản để tiếp tục.'),
 });
 
-const handleRegister = () => {
-  console.log(form.value);
-}
+const isPasswordVisible = ref(false);
+const isConfirmPasswordVisible = ref(false);
+const registerNotification = ref({
+  state: false,
+  message: "",
+});
+
+const schema = yup.object({
+  email: yup
+    .string()
+    .required("Email không được để trống")
+    .email("Email không đúng định dạng")
+    .matches(/@gmail\.com$/, "Email phải có định dạng là @gmail.com"),
+  userName: yup.string().required("Tên người dùng không được để trống"),
+  password: yup
+    .string()
+    .required("Mật khẩu không được để trống")
+    .min(8, "Mật khẩu cần có tối thiểu 8 kí tự")
+    .matches(
+      /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
+      "Mật khẩu yêu cầu chứa cả chữ và số"
+    ),
+  confirmPassword: yup
+    .string()
+    .required("Xác nhận mật khẩu là bắt buộc")
+    .oneOf([yup.ref("password"), ""], "Xác nhận mật khẩu không chính xác"),
+  agree: yup
+    .boolean()
+    .oneOf([true], "Bạn cần phải đồng ý với các điều kiện và điều khoản để tiếp tục."),
+});
+
+const handleRegister = async () => {
+  try {
+    // await axiosClient.post(`${API_ENDPOINT}/register`, form.value);
+    registerNotification.value.state = true;
+    registerNotification.value.message = "Đăng kí tài khoản thành công";
+  } catch (error) {
+    registerNotification.value.message = "Đăng kí tài khoản thất bại";
+  }
+};
 </script>
 
 <template>
-  <div class="auth-wrapper d-flex align-center justify-center pa-4 container-card-login ">
+  <div class="auth-wrapper d-flex align-center justify-center pa-4 container-card-login">
     <VCard class="auth-card pa-4 pt-7 login-image-card" width="1000" height="75%"></VCard>
 
     <VCard class="auth-card pa-4 pt-7 login-form-card" max-width="600" height="75%">
-
-      <VCardItem class="justify-center ">
-        <VCardTitle class="text-2xl ">
-          <p class="login-title-bot">
-            MUSIC & COFFEE
-          </p>
+      <VCardItem class="justify-center">
+        <VCardTitle class="text-2xl">
+          <p class="login-title-bot">MUSIC & COFFEE</p>
         </VCardTitle>
       </VCardItem>
 
       <VCardText class="pt-2">
-        <h3 class="mb-0 mt-5">
-          Hãy tiến hành tạo tài khoản để trải nghiệm hệ thống
-        </h3>
+        <h3 class="mb-0 mt-5">Hãy tiến hành tạo tài khoản để trải nghiệm hệ thống</h3>
       </VCardText>
 
       <VCardText>
@@ -60,52 +74,78 @@ const handleRegister = () => {
             <!-- email -->
             <VCol cols="12">
               <Field name="email" v-model="form.email" v-slot="{ handleChange }">
-                <VTextField @input="handleChange" v-model="form.email" autofocus placeholder="Nhập email của bạn "
-                  label="Email" type="email" />
+                <VTextField
+                  @input="handleChange"
+                  v-model="form.email"
+                  autofocus
+                  placeholder="Nhập email của bạn "
+                  label="Email"
+                  type="email"
+                />
                 <ErrorMessage name="email" class="show-error-message" />
               </Field>
             </VCol>
 
             <VCol cols="12">
               <Field name="userName" v-model="form.userName" v-slot="{ handleChange }">
-                <VTextField @input="handleChange" v-model="form.userName" label="Tên người dùng"
-                  placeholder="Nhập tên người dùng" type="text" />
+                <VTextField
+                  @input="handleChange"
+                  v-model="form.userName"
+                  label="Tên người dùng"
+                  placeholder="Nhập tên người dùng"
+                  type="text"
+                />
                 <ErrorMessage name="userName" class="show-error-message" />
               </Field>
             </VCol>
 
             <VCol cols="12">
               <Field name="password" v-model="form.password" v-slot="{ handleChange }">
-                <VTextField @input="handleChange" v-model="form.password" label="Mật khẩu" placeholder="Nhập mật khẩu"
+                <VTextField
+                  @input="handleChange"
+                  v-model="form.password"
+                  label="Mật khẩu"
+                  placeholder="Nhập mật khẩu"
                   :type="isPasswordVisible ? 'text' : 'password'"
                   :append-inner-icon="isPasswordVisible ? 'bx-hide' : 'bx-show'"
-                  @click:append-inner="isPasswordVisible = !isPasswordVisible" />
+                  @click:append-inner="isPasswordVisible = !isPasswordVisible"
+                />
                 <ErrorMessage name="password" class="show-error-message" />
               </Field>
             </VCol>
 
             <VCol cols="12">
-              <Field name="confirmPassword" v-model="form.confirm" v-slot="{ handleChange }">
-                <VTextField @input="handleChange" label="Xác nhận mật khẩu" placeholder="Xác nhận mật khẩu"
+              <Field
+                name="confirmPassword"
+                v-model="form.confirm"
+                v-slot="{ handleChange }"
+              >
+                <VTextField
+                  @input="handleChange"
+                  label="Xác nhận mật khẩu"
+                  placeholder="Xác nhận mật khẩu"
                   :type="isConfirmPasswordVisible ? 'text' : 'password'"
                   :append-inner-icon="isConfirmPasswordVisible ? 'bx-hide' : 'bx-show'"
-                  @click:append-inner="isConfirmPasswordVisible = !isConfirmPasswordVisible" />
+                  @click:append-inner="
+                    isConfirmPasswordVisible = !isConfirmPasswordVisible
+                  "
+                />
                 <ErrorMessage name="confirmPassword" class="show-error-message" />
               </Field>
 
               <div class="d-flex align-center justify-space-between flex-wrap mt-3 mb-4">
                 <Field name="agree" v-model="form.agree" v-slot="{ handleChange }">
-                  <VCheckbox v-model="form.agree"
-                    label="Tôi đồng ý với các điều kiện và điều khoản khi sử dụng hệ thống" />
+                  <VCheckbox
+                    v-model="form.agree"
+                    label="Tôi đồng ý với các điều kiện và điều khoản khi sử dụng hệ thống"
+                  />
                   <ErrorMessage name="agree" class="show-error-message" />
-
                 </Field>
               </div>
 
-              <VBtn block class="mt-5" @click="handleRegister" :disabled="!meta.valid">
+              <VBtn block class="mt-5" @click="handleRegister" :disabled="meta.valid">
                 Đăng kí
               </VBtn>
-
             </VCol>
 
             <VCol cols="12" class="text-center text-base">
@@ -118,10 +158,20 @@ const handleRegister = () => {
         </Form>
       </VCardText>
     </VCard>
+    <SnackbarCustom
+      :isShow="registerNotification.state"
+      :message="registerNotification.message"
+      :onClose="
+        () => {
+          registerNotification.state = false;
+        }
+      "
+    >
+    </SnackbarCustom>
   </div>
 </template>
 
-<style lang="scss" >
+<style lang="scss">
 @use "@core/scss/template/pages/page-auth.scss";
 
 .login-sercives .v-btn__content {
@@ -149,7 +199,8 @@ const handleRegister = () => {
 }
 
 .login-image-card {
-  background: url('https://i.pinimg.com/originals/2d/9a/99/2d9a99324ede6e2787db071ffeae8ca9.gif') center center no-repeat !important;
+  background: url("https://i.pinimg.com/originals/2d/9a/99/2d9a99324ede6e2787db071ffeae8ca9.gif")
+    center center no-repeat !important;
   background-size: cover !important;
   width: 100%;
   height: 300px;
@@ -176,7 +227,7 @@ const handleRegister = () => {
 
 .login-title-bot {
   font-size: 46px !important;
-  color: #6358DC;
+  color: #6358dc;
   font-weight: 900;
   font-family: inherit;
 }
